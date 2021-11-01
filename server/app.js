d2f1a34b1f8d3f8ff84fcd7f79134356908fa2e4
Viktoria Ihnatsik <ignatik_viktoria@hotmail.com>
2021-11-01 14:47:55 +0100

    Connected with mongoDB

--- server/app.js
+++ server/app.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-
 const indexRouter = require('./routes/index');
 
 const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(logger('dev'));
@@ -17,5 +18,20 @@ app.use(cookieParser());
 
 
 app.use('/', indexRouter);
+
+async function start() {
+ try {
+  await mongoose.connect('mongodb+srv://admin:admin@cluster0.rywg4.mongodb.net/todo?retryWrites=true&w=majority', {
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+  })
+
+  app.listen(port, () => {
+   console.log(`Server started on port ${port}`)
+  })
+
+ } catch(err) {console.error(err)}
+}
+start();
 
 module.exports = app;
