276356089157fe90aae9a1c9e9bc998ca9f90752
Viktoria Ihnatsik <ignatik_viktoria@hotmail.com>
2021-11-02 16:08:12 +0100

    Started to work with routing on client

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
+import React from "react";
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import TodoPage from "./pages/TodoPage";
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className="container text-center mt-5">
+      <p>Hello</p>
+    
+      <Switch> 
+
+        <Route path="/" component={TodoPage}> 
+        
+        </Route>
+
+      </Switch>
+
     </div>
   );
 }
