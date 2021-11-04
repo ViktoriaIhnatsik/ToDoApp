91987eb570916e4f26405c4e3bafc8f170b5501e
Viktoria Ihnatsik <ignatik_viktoria@hotmail.com>
2021-11-04 20:28:44 +0100

    Added TodoFormComponent

--- client/src/pages/TodoPage.jsx
+++ client/src/pages/TodoPage.jsx
@@ -1,47 +1,32 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "../App.css";
 import TodoComponent from "../components/TodoComponent";
+import TodoFormComponent from "../components/TodoFormComponent";
 
 export default function TodoPage() {
   const [todos, setTodos] = useState([]);
 
   useEffect(() => {
-    getTodos()
+    getTodos();
   }, []);
 
   function getTodos() {
     const url = "http://localhost:5000/todo/";
     fetch(url)
-    .then(res => res.json())
-    .then(data => setTodos(data))
-  };
- 
+      .then((res) => res.json())
+      .then((data) => setTodos(data));
+  }
 
   return (
     <div className="container">
       <h3 className="text-center">Add todo</h3>
-      <form className="form">
-        <div className="mb-3">
-          <label className="form-label">Title</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Add title..."
-          />
-        </div>
-        <div className="mb-3">
-          <label className="form-label">Content</label>
-          <textarea className="form-control" />
-        </div>
-        <button type="submit" className="btn btn-secondary">
-          Add
-        </button>
-      </form>
+
+      <TodoFormComponent />
 
       <div className="row">
-        {todos.map(item => { 
-            return <TodoComponent key={item.id} todo={item} />;
-          })}
+        {todos.map((item) => {
+          return <TodoComponent key={item.id} todo={item} />;
+        })}
       </div>
     </div>
   );
