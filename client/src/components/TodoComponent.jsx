7dd1e8bd1a3d6c5f313f551efe989b56f04d0eb2
Viktoria Ihnatsik <ignatik_viktoria@hotmail.com>
2021-11-05 14:16:06 +0100

    Added delete on client

--- client/src/components/TodoComponent.jsx
+++ client/src/components/TodoComponent.jsx
@@ -1,15 +1,32 @@
 import React from "react";
 
-export default function TodoComponent({todo}) {
+ const deleteTodo = (id) => {
+   const url = `http://localhost:5000/todo/${id}`;
+   fetch(url, {
+     method: "DELETE",
+     headers: {
+       "Content-Type": "application/json",
+     },
+   })
+   .then(window.location.reload())
+ };
+
+
+export default function TodoComponent({ todo }) {
   return (
-    <div className="col-md-3 border p-3 m-5" >
+    <div className="col-md-3 border p-3 m-5">
       <h3>{todo.title}</h3>
       <p>{todo.content}</p>
+      <p>Last updated:{todo.updatedAt}</p>
       <div className="text-end">
         <button type="button" className="btn btn-outline-warning btn-sm m-2">
           Edit
         </button>
-        <button type="button" className="btn btn-outline-danger btn-sm">
+        <button
+          type="button"
+          className="btn btn-outline-danger btn-sm"
+         onClick={() => deleteTodo(todo._id)}
+        >
           Delete
         </button>
       </div>
