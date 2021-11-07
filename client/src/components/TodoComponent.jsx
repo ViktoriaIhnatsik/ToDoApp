aa4999d398419ac5146f3bbc135cc27f21f0f566
Viktoria Ihnatsik <ignatik_viktoria@hotmail.com>
2021-11-07 13:50:49 +0100

    Added modal for edit

--- client/src/components/TodoComponent.jsx
+++ client/src/components/TodoComponent.jsx
@@ -1,34 +1,71 @@
 import React from "react";
-
- const deleteTodo = (id) => {
-   const url = `http://localhost:5000/todo/${id}`;
-   fetch(url, {
-     method: "DELETE",
-     headers: {
-       "Content-Type": "application/json",
-     },
-   })
-   .then(window.location.reload())
- };
-
+import { useState } from "react";
+import Modal from "react-modal";
+import "../App.css";
 
 export default function TodoComponent({ todo }) {
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  const deleteTodo = (id) => {
+    const url = `http://localhost:5000/todo/${id}`;
+    fetch(url, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(window.location.reload());
+  };
+
   return (
     <div className="col-md-3 border p-3 m-5">
       <h3>{todo.title}</h3>
       <p>{todo.content}</p>
       <p>Last updated:{todo.updatedAt}</p>
       <div className="text-end">
-        <button type="button" className="btn btn-outline-warning btn-sm m-2">
+        <button
+          type="button"
+          className="btn btn-outline-warning btn-sm m-2"
+          onClick={openModal}
+        >
           Edit
         </button>
         <button
           type="button"
           className="btn btn-outline-danger btn-sm"
-         onClick={() => deleteTodo(todo._id)}
+          onClick={() => deleteTodo(todo._id)}
         >
           Delete
         </button>
+      </div>
+
+      <div>
+        <Modal isOpen={modalIsOpen} onRequestClose={closeModal}>
+          <h3>{todo.title}</h3>
+          <p>{todo.content}</p>
+          <p>Last updated:{todo.updatedAt}</p>
+          <button
+            type="button"
+            className="btn btn-outline-secondary btn-sm m-2"
+            onClick={closeModal}
+          >
+            Close
+          </button>
+          <button
+            type="button"
+            className="btn btn-outline-danger btn-sm"
+            onClick={() => deleteTodo(todo._id)}
+          >
+            Delete
+          </button>
+        </Modal>
       </div>
     </div>
   );
