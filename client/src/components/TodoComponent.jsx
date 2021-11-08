7586935d4d9ae4718fb64a613851841395776c5e
Viktoria Ihnatsik <ignatik_viktoria@hotmail.com>
2021-11-08 12:40:09 +0100

    Added edit function

--- client/src/components/TodoComponent.jsx
+++ client/src/components/TodoComponent.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { useState } from "react";
 import Modal from "react-modal";
-import "../App.css";
+Modal.setAppElement(document.getElementById("root"));
 
 export default function TodoComponent({ todo }) {
   const [modalIsOpen, setIsOpen] = useState(false);
+  const [editedTitle, setEditedTitle] = useState("");
+  const [editedContent, setEditedContent] = useState("");
 
   function openModal() {
     setIsOpen(true);
@@ -13,6 +15,18 @@ export default function TodoComponent({ todo }) {
   function closeModal() {
     setIsOpen(false);
   }
+
+  const editTodo = (e) => {
+    e.preventDefault();
+    const url = `http://localhost:5000/todo/${todo._id}`;
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({ editedTitle, editedContent }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(window.location.reload());
+  };
 
   const deleteTodo = (id) => {
     const url = `http://localhost:5000/todo/${id}`;
@@ -48,23 +62,50 @@ export default function TodoComponent({ todo }) {
 
       <div>
         <Modal isOpen={modalIsOpen} onRequestClose={closeModal}>
-          <h3>{todo.title}</h3>
-          <p>{todo.content}</p>
-          <p>Last updated:{todo.updatedAt}</p>
-          <button
-            type="button"
-            className="btn btn-outline-secondary btn-sm m-2"
-            onClick={closeModal}
-          >
-            Close
-          </button>
-          <button
-            type="button"
-            className="btn btn-outline-danger btn-sm"
-            onClick={() => deleteTodo(todo._id)}
-          >
-            Delete
-          </button>
+          <form className="form" onSubmit={editTodo}>
+            <div className="mb-3">
+              <label className="form-label">Title</label>
+              <input
+                type="text"
+                className="form-control"
+                defaultValue={todo.title}
+                onChange={(e) => {
+                  setEditedTitle(e.target.value);
+                }}
+              />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Content</label>
+              <textarea
+                className="form-control"
+                defaultValue={todo.content}
+                onChange={(e) => {
+                  setEditedContent(e.target.value);
+                }}
+              />
+            </div>
+
+            <button
+              type="button"
+              className="btn btn-outline-secondary btn-sm "
+              onClick={closeModal}
+            >
+              Close
+            </button>
+            <button
+              className="btn btn-outline-success btn-sm m-2"
+              type="submit"
+            >
+              Save
+            </button>
+            <button
+              type="button"
+              className="btn btn-outline-danger btn-sm"
+              onClick={() => deleteTodo(todo._id)}
+            >
+              Delete
+            </button>
+          </form>
         </Modal>
       </div>
     </div>
