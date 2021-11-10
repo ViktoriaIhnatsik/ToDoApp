75d0256708bdd0175604ace45c47708666e6aa37
Viktoria Ihnatsik <ignatik_viktoria@hotmail.com>
2021-11-10 14:52:06 +0100

    Added error messages

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,41 +5,68 @@ const Todo = require('../models/todoModel');
 
 // get all items
 router.get('/', async (req, res) => {
+  try {
   const todos = await Todo.find({});
   res.status(200).json(todos);
+  }
+  catch(err) {
+    res.status(400).json({message: err.message});
+  }
   });
 
 //get specific item
 router.get('/:id', async (req, res) => {
+  try {
   const todo = await Todo.findOne({_id: req.params.id});
   res.status(200).json(todo);
+  }
+  catch(err) {
+    res.status(400).json({message: err.message});
+  }
 });
 
 // create ny item
 router.post('/', async (req, res) => {
   const { title, content } = req.body;
+  try {
   const newTodo = await Todo.create({
     title,
     content
   });
   res.status(200).json(newTodo);
+  }
+   catch(err) {
+    res.status(400).json({message: err.message});
+  }
+
 });
 
 // update item
 router.post('/:id', async (req, res) => {
  const {  editedTitle, editedContent } = req.body;
  const id = req.params.id;
+ try {
  const updatedTodo = await Todo.findByIdAndUpdate( id,{
     title: editedTitle,
     content: editedContent,
  });
    res.status(200).json(updatedTodo);
+}
+catch(err) {
+    res.status(400).json({message: err.message});
+  }
+
 });
 
 // delete item
 router.delete('/:id', async (req, res) => {
+  try {
   const todo = await Todo.findOneAndDelete({_id: req.params.id});
   res.status(204).json(null)
+  }
+  catch(err) {
+    res.status(400).json({message: err.message});
+  }
 });
 
 module.exports = router;
